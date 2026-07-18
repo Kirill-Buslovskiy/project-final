6b8504b3d0e4a57e300d09cc2ed486354f337503
Kirill Buslovskii <k.buslovsky@yandex.ru>
2026-07-18 19:42:48 +0300

    add feature "addTag"

--- src/main/java/com/javarush/jira/bugtracking/task/TaskController.java
+++ src/main/java/com/javarush/jira/bugtracking/task/TaskController.java
@@ -156,4 +156,10 @@ public class TaskController {
             this(taskTo, new LinkedList<>());
         }
     }
+
+    @PostMapping("/{id}/tags")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void addTag(@PathVariable long id, @RequestBody String tag) {
+        taskService.addTag(id, tag.replace("\"", ""));
+    }
 }
